a2decfca9f9a697ea1d609feb8f90f88c40a831e
Kamran Azim <Kamazim121212@gmail.com>
2024-03-06 00:27:13 +0000

    User can now turn lef, right and backwards in order to sketch

--- main.py
+++ main.py
@@ -4,9 +4,20 @@ pen = t.Turtle()
 screen = t.Screen()
 
 def move():
-    pen.forward(2)
+    pen.forward(5)
+
+def turn_left():
+    pen.left(5)
+
+def turn_right():
+    pen.right(5)
+
+def turn_back():
+    pen.back(5)
 
 screen.listen()
 screen.onkeypress(key = "w", fun=move)
-
+screen.onkeypress(key = "a", fun=turn_left)
+screen.onkeypress(key = "d", fun=turn_right)
+screen.onkeypress(key = "s", fun=turn_back)
 screen.exitonclick()

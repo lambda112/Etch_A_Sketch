41b203c4645ad830fd74bd72c78130baa8fd9caa
Kamran Azim <Kamazim121212@gmail.com>
2024-03-06 00:39:58 +0000

    Finished project. Allowed user to reset with c button to clear drawing and go back to original position

--- main.py
+++ main.py
@@ -4,20 +4,25 @@ pen = t.Turtle()
 screen = t.Screen()
 
 def move():
-    pen.forward(5)
+    pen.forward(10)
 
 def turn_left():
-    pen.left(5)
+    pen.left(10)
 
 def turn_right():
-    pen.right(5)
+    pen.right(10)
 
 def turn_back():
-    pen.back(5)
+    pen.back(10)
+
+def clear_screen():
+    pen.reset()
+
 
 screen.listen()
 screen.onkeypress(key = "w", fun=move)
 screen.onkeypress(key = "a", fun=turn_left)
 screen.onkeypress(key = "d", fun=turn_right)
 screen.onkeypress(key = "s", fun=turn_back)
+screen.onkeypress(key = "c", fun=clear_screen)
 screen.exitonclick()
